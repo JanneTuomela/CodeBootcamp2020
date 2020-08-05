e46a16212fa468aa0608cc9a2a0e4d0f3bf841cd
JanneTuomela <janne_tuomela@hotmail.com>
2020-08-05 14:37:16 +0300

    Harjoitus1.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/oma")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public int Luku()
+        { return 123; }
+
+    }
+}

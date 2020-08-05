c3d9af0e6171229767bdd3de70c33aa608715533
JanneTuomela <janne_tuomela@hotmail.com>
2020-08-05 17:28:55 +0300

    Lisätty otteluiden lopputulosten arvonta.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/ValuesController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreBackend.Controllers
@@ -14,6 +15,15 @@ namespace AspNetCoreBackend.Controllers
         [Route("luku")]
         public int Luku()
         { return 123; }
+
+        [Route("satunnainen")]
+        public int Satunnaisluku()
+        {
+            Random random = new Random();
+            int luku = random.Next(1, 20);
+            return luku;
+        }
+
         [Route("merkkijono")]
         public string Merkkijono()
         { return "ABCD"; }
@@ -22,5 +32,55 @@ namespace AspNetCoreBackend.Controllers
         {
             return new string[] { "ABCD", "DEFG" };
         }
+        [Route("ottelut")]
+        public List<string> Ottelut()
+        {
+            string[] joukkueet = new string[]
+            {
+                "HIFK", "HPK", "Ilves", "Jukurit", "JYP", "KalPa", "KooKoo", "Kärpät", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Ässät"
+            };
+            string[] maalit = new string[]
+                { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+            List<string> pelit = new List<string>();
+            Random random = new Random();
+
+            for (int laskuri = 0; laskuri < 210; laskuri++)
+
+            {
+                bool peliOk = false;
+                while (!peliOk)
+                {
+                    int indeksi = random.Next(0, joukkueet.Length);
+                    int indeksi3 = random.Next(0, maalit.Length);
+                    string kotiJoukkue = joukkueet[indeksi];
+                    string kotiMaalit = maalit[indeksi3];
+
+                    bool indeksiOk = false;
+                    int indeksi2 = random.Next(0,joukkueet.Length);
+                    int indeksi4 = random.Next(0, maalit.Length);
+                    while (!indeksiOk)
+                    {
+                        indeksi2 = random.Next(0, joukkueet.Length);
+                        indeksiOk = indeksi != indeksi2;
+                        indeksi4 = random.Next(0, maalit.Length);
+                    }
+                    string vierasJoukkue = joukkueet[indeksi2];
+                    string vierasMaalit = maalit[indeksi4];
+
+                    string peli = kotiJoukkue + "-" + vierasJoukkue + " " + kotiMaalit + "-" + vierasMaalit;
+
+                    if (!pelit.Contains(peli))
+                    {
+                        pelit.Add(peli);
+                        peliOk = true;
+                    }
+                }
+            }
+
+            return pelit;
+        }
+
     }
+
 }

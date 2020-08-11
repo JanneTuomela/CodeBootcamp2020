88dac8041c841afad679557934928faf7c680d39
JanneTuomela <janne_tuomela@hotmail.com>
2020-08-11 15:05:49 +0300

    Lisätty pistelaskentaa ja sarjataulukkoa järjestyksineen.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/ValuesController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/ValuesController.cs
@@ -35,17 +35,25 @@ namespace AspNetCoreBackend.Controllers
 
         [Route("ottelut")]
         public List<string> Ottelut()
+      
         {
             string[] joukkueet = new string[]
             {
                 "HIFK", "HPK", "Ilves", "Jukurit", "JYP", "KalPa", "KooKoo", "Kärpät", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Ässät"
             };
-            string[] maalit = new string[]
-                { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            int[] maalit = new int[]
+                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+            Dictionary<string, int> pistetilanne = new Dictionary<string, int>();
+            
             List<string> pelit = new List<string>();
             List<string> tulokset = new List<string>();
             Random random = new Random();
+
+            foreach (string joukkue in joukkueet)
+            {
+                pistetilanne.Add(joukkue, 0);
+            }
 
             for (int laskuri = 0; laskuri < 210; laskuri++)
 
@@ -56,7 +64,7 @@ namespace AspNetCoreBackend.Controllers
                     int indeksi = random.Next(0, joukkueet.Length);
                     int indeksi3 = random.Next(0, maalit.Length);
                     string kotiJoukkue = joukkueet[indeksi];
-                    string kotiMaalit = maalit[indeksi3];
+                    int kotiMaalit = maalit[indeksi3];
 
                     bool indeksiOk = false;
                     int indeksi2 = random.Next(0,joukkueet.Length);
@@ -67,22 +75,71 @@ namespace AspNetCoreBackend.Controllers
                         indeksiOk = indeksi != indeksi2;
                     }
                     string vierasJoukkue = joukkueet[indeksi2];
-                    string vierasMaalit = maalit[indeksi4];
+                    int vierasMaalit = maalit[indeksi4];
 
                     string peli = kotiJoukkue + "-" + vierasJoukkue;
                     string lopputulos = kotiMaalit + "-" + vierasMaalit;
 
+                   
+                   
                     if (!pelit.Contains(peli))
                     {
                         pelit.Add(peli + " " + lopputulos);
                         //tulokset.Add(lopputulos);
                         peliOk = true;
+
+                        if (kotiMaalit > vierasMaalit)
+                        {
+                            pistetilanne[kotiJoukkue] += 3;
+                            pistetilanne[vierasJoukkue] += 0;
+                        }
+
+                        else if (vierasMaalit > kotiMaalit)
+                        {
+                            pistetilanne[kotiJoukkue] += 0;
+                            pistetilanne[vierasJoukkue] += 3;
+                        }
+                        else
+                        {
+                            pistetilanne[kotiJoukkue] += 1;
+                            pistetilanne[vierasJoukkue] += 1;
+                        }
+
+
                     }
+                    
                 }
+                
+            }
+            static void Lokiin(string viesti)
+            {
+                const string TiedostoNimi = "C:\\users\\janne\\Sarjataulukko.txt";
+                System.IO.File.AppendAllText(TiedostoNimi, viesti + "\r\n");
             }
 
+
+            var Sarjataulukko = pistetilanne.ToList();
+            Sarjataulukko.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value));
+            Sarjataulukko.ToArray();
+            Lokiin("SARJATAULUKKO:\n");
+            
+            foreach (KeyValuePair<string, int> kvp in Sarjataulukko)
+            {
+                Lokiin("Joukkueen " + kvp.Key + " pistetilanne: " + kvp.Value);
+            }
+
+            /*
+            foreach (string joukkue in joukkueet)
+            {
+
+
+                Lokiin("SARJATAULUKKO\n" + "Joukkueen " + joukkue + " pistetilanne: " + Sarjataulukko);
+            }*/
+            
             return pelit;
 
+          
+            
         }   
 
     }
